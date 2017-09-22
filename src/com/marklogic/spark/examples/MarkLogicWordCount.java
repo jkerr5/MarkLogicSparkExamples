e9f6a895ce6acdabc15e708d5bf59e7f116b884a
jkerr <james.kerr@marklogic.com>
2017-09-22 17:15:57 -0400

    Fixed formatting

--- src/com/marklogic/spark/examples/MarkLogicWordCount.java
+++ src/com/marklogic/spark/examples/MarkLogicWordCount.java
@@ -37,109 +37,108 @@ import java.util.Iterator;
 public class MarkLogicWordCount {
 
     private static final PairFlatMapFunction<Tuple2<DocumentURI, MarkLogicNode>, String, String> ELEMENT_NAME_VALUE_PAIR_EXTRACTOR =
-            new PairFlatMapFunction<Tuple2<DocumentURI,MarkLogicNode>, String, String>() {
-                @Override
-                public Iterator<Tuple2<String, String>> call(Tuple2<DocumentURI, MarkLogicNode> arg) throws Exception {
-                    DocumentURI key = arg._1();
-                    MarkLogicNode value = arg._2();
-                    ArrayList<Tuple2<String, String>> elementValuePairs = new ArrayList<Tuple2<String, String>>();
-                    if (key != null && value != null && value.get() != null &&
-                            value.get().getNodeType() == Node.DOCUMENT_NODE) {
-                        Document doc = (Document)value.get();
-                        Element docElement = doc.getDocumentElement();
-                        NodeList childNodes = docElement.getChildNodes();
-                        for(int index=0; index < childNodes.getLength(); index++){
-                            Node nodeItem = childNodes.item(index);
-			    if (nodeItem.getNodeType() == Node.ELEMENT_NODE) {
-				elementValuePairs.add(new Tuple2<String, String>(nodeItem.getNodeName(), nodeItem.getTextContent()));
-			    }
+        new PairFlatMapFunction<Tuple2<DocumentURI,MarkLogicNode>, String, String>() {
+            @Override
+            public Iterator<Tuple2<String, String>> call(Tuple2<DocumentURI, MarkLogicNode> arg) throws Exception {
+                DocumentURI key = arg._1();
+                MarkLogicNode value = arg._2();
+                ArrayList<Tuple2<String, String>> elementValuePairs = new ArrayList<Tuple2<String, String>>();
+                if (key != null && value != null && value.get() != null &&
+                        value.get().getNodeType() == Node.DOCUMENT_NODE) {
+                    Document doc = (Document)value.get();
+                    Element docElement = doc.getDocumentElement();
+                    NodeList childNodes = docElement.getChildNodes();
+                    for(int index=0; index < childNodes.getLength(); index++){
+                        Node nodeItem = childNodes.item(index);
+                        if (nodeItem.getNodeType() == Node.ELEMENT_NODE) {
+                            elementValuePairs.add(new Tuple2<String, String>(nodeItem.getNodeName(), nodeItem.getTextContent()));
                         }
-                    } else {
-                        System.out.println("Error in FlatMapFunction key: " + key + ", value: " + value);
                     }
-                    return elementValuePairs.iterator();
+                } else {
+                    System.out.println("Error in FlatMapFunction key: " + key + ", value: " + value);
                 }
-            };
+                return elementValuePairs.iterator();
+            }
+        };
 
     private static final PairFunction<Tuple2<String, String>, String, Integer> ELEMENT_VALUE_OCCURRENCE_COUNT_MAPPER =
-            new PairFunction<Tuple2<String, String>, String, Integer>() {
-                @Override
-                public Tuple2<String, Integer> call(Tuple2<String, String> pair) throws Exception {
-                    String nameValueOccurrence = pair._1().concat(":").concat(pair._2());
-                    if(nameValueOccurrence.length() > 0) {
-                        return new Tuple2<String, Integer>(nameValueOccurrence, 1);
-                    }else{
-                        return new Tuple2<String, Integer>(nameValueOccurrence, 0);
-                    }
+        new PairFunction<Tuple2<String, String>, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(Tuple2<String, String> pair) throws Exception {
+                String nameValueOccurrence = pair._1().concat(":").concat(pair._2());
+                if(nameValueOccurrence.length() > 0) {
+                    return new Tuple2<String, Integer>(nameValueOccurrence, 1);
+                }else{
+                    return new Tuple2<String, Integer>(nameValueOccurrence, 0);
                 }
-            };
+            }
+        };
 
     private static final Function2<Integer, Integer, Integer> ELEMENT_VALUE_COUNT_REDUCER =
-            new Function2<Integer, Integer, Integer>() {
-                @Override
-                public Integer call(Integer a, Integer b) throws Exception {
-                    return a + b;
-                }
-            };
+        new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer a, Integer b) throws Exception {
+                return a + b;
+            }
+        };
 
     private static final Function<Tuple2<String, Integer>, Boolean> ELEMENT_VALUE_COUNT_FILTER =
-            new Function<Tuple2<String, Integer>, Boolean>() {
-                @Override
-                public Boolean call(Tuple2<String, Integer> arg) throws Exception {
-                    return arg._2() > 1;
-                }
-            };
+        new Function<Tuple2<String, Integer>, Boolean>() {
+            @Override
+            public Boolean call(Tuple2<String, Integer> arg) throws Exception {
+                return arg._2() > 1;
+            }
+        };
 
     private static final Function<Iterable<String>, Integer> DISTINCT_VALUE_COUNTER =
-            new Function<Iterable<String>, Integer>() {
-                @Override
-                public Integer call(Iterable<String> values) throws Exception {
-                    HashSet<String> distinctValues = new HashSet<String>();
-                    Iterator<String> it = values.iterator();
-                    while(it.hasNext()){
-                        distinctValues.add(it.next());
-                    }
-                    return distinctValues.size();
+        new Function<Iterable<String>, Integer>() {
+            @Override
+            public Integer call(Iterable<String> values) throws Exception {
+                HashSet<String> distinctValues = new HashSet<String>();
+                Iterator<String> it = values.iterator();
+                while(it.hasNext()){
+                    distinctValues.add(it.next());
                 }
-            };
+                return distinctValues.size();
+            }
+        };
 
     public static class WritableSerializer extends Serializer<Writable> {
         @Override
         public void write(Kryo kryo, Output output, Writable obj) {
-          try {
-	      obj.write(new DataOutputStream(output));
-	  } catch (IOException e) {
-	      throw new KryoException(e);
-	  }
+            try {
+                obj.write(new DataOutputStream(output));
+            } catch (IOException e) {
+                throw new KryoException(e);
+            }
         }
 
         @Override
         public Writable read(Kryo kryo, Input input, Class<Writable> aClass) {
-	  try {
-	      Writable obj = aClass.newInstance();
-	      obj.readFields(new DataInputStream(input));
-	      return obj;
-	  } catch (IllegalAccessException e) {
-	      throw new KryoException(e);
-	  } catch (InstantiationException e) {
-	      throw new KryoException(e);
-	  } catch (IOException e) {
-	      throw new KryoException(e);
-	  }          
+            try {
+                Writable obj = aClass.newInstance();
+                obj.readFields(new DataInputStream(input));
+                return obj;
+            } catch (IllegalAccessException e) {
+                throw new KryoException(e);
+            } catch (InstantiationException e) {
+                throw new KryoException(e);
+            } catch (IOException e) {
+                throw new KryoException(e);
+            }
         }
     }
 
     public static class MarkLogicKryoRegistrator implements KryoRegistrator {
-      public void registerClasses(Kryo kryo) {
-	// we could cover all Writable objects using this but we will just limit it to the MarkLogic classes
-	// we know need a custom serializer for now
+        public void registerClasses(Kryo kryo) {
+            // we could cover all Writable objects using this but we will just limit it to the MarkLogic classes
+            // we know need a custom serializer for now
 
-        Serializer serializer = new WritableSerializer();
-	kryo.register(MarkLogicNode.class, serializer);
-        kryo.register(DocumentURI.class, serializer);
-      }
+            Serializer serializer = new WritableSerializer();
+            kryo.register(MarkLogicNode.class, serializer);
+            kryo.register(DocumentURI.class, serializer);
+        }
     }
-
 
     public static void main(String[] args) throws IOException {
         if (args.length < 2) {
@@ -149,12 +148,12 @@ public class MarkLogicWordCount {
 
         // first you create the spark context within java
         SparkConf conf = new SparkConf()
-          .setAppName("com.marklogic.spark.examples")
-          // Spark uses Java serialization as the default serializer but the MarkLogic
-          // hadoop classes implement org.apache.hadoop.io.Writable but not java.io.Serializable
-          // so we need to use the Kryo serializer
-          .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-          .set("spark.kryo.registrator", MarkLogicKryoRegistrator.class.getName())
+            .setAppName("com.marklogic.spark.examples")
+            // Spark uses Java serialization as the default serializer but the MarkLogic
+            // hadoop classes implement org.apache.hadoop.io.Writable but not java.io.Serializable
+            // so we need to use the Kryo serializer
+            .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+            .set("spark.kryo.registrator", MarkLogicKryoRegistrator.class.getName())
         ;
 
         JavaSparkContext context = new JavaSparkContext(conf);
@@ -166,14 +165,15 @@ public class MarkLogicWordCount {
         hdConf.addResource(ipStream);
         //Create RDD based on documents within MarkLogic database. Load documents as DocumentURI, MarkLogicNode pairs.
         JavaPairRDD<DocumentURI, MarkLogicNode> mlRDD = context.newAPIHadoopRDD(
-                hdConf,                     //Configuration
-                DocumentInputFormat.class,  //InputFormat
-                DocumentURI.class,          //Key Class
-                MarkLogicNode.class         //Value Class
+            hdConf,                     //Configuration
+            DocumentInputFormat.class,  //InputFormat
+            DocumentURI.class,          //Key Class
+            MarkLogicNode.class         //Value Class
         );
 
-	// Use a repartitioning stage to test out the serialization of MarkLogicNode objects
-	mlRDD = mlRDD.repartition(8);
+        // Use a repartitioning stage to test out the serialization of MarkLogicNode objects
+        // this makes the job run a lot slower as it has to serialize/deserialize all the XML
+        //mlRDD = mlRDD.repartition(8);
 
         //extract XML elements as name value pairs where element content is value
         JavaPairRDD<String, String> elementNameValuePairs = mlRDD.flatMapToPair(ELEMENT_NAME_VALUE_PAIR_EXTRACTOR);
